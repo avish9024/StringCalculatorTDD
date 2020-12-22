23a14e576fc469d99263dff58f13c2bd4b3208bf
Avinash Kumar <avinash9024@gmail.com>
2020-12-22 23:47:35 +0530

    strings having new lines

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,18 +2,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StringCalculator {
-    public int add(String numbers) {
+    public static int add(String numbers) {
         if(numbers.length() == 0){
             return 0;
         }
         if(numbers.length() == 1) {
             return Integer.parseInt(numbers);
         }
+        if (numbers.contains("\n")) {
+            List<String> stringListWithoutNewLineAndComma = Arrays
+                    .asList(numbers.replaceAll("\n", ",").split(","));
+            return getSum(stringListWithoutNewLineAndComma);
+        }
         List<String> numbersList = Arrays.asList(numbers.split(","));
         return getSum(numbersList);
     }
 
-    public int getSum(List<String> numbersList) {
+    public static int getSum(List<String> numbersList) {
         int sum = 0;
         for (String number: numbersList) {
             sum += Integer.parseInt(number);
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -3,21 +3,25 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+
     @Test
     public void testEmptyString() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(0, calculator.add(""));
+        assertEquals(0, StringCalculator.add(""));
     }
 
     @Test
     public void testStringHavingOneNumber() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(1, calculator.add("1"));
+        assertEquals(1, StringCalculator.add("1"));
     }
 
     @Test
     public void testStringHavingTwoNumbers() {
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void testStringHavingNewLines() {
         StringCalculator calculator = new StringCalculator();
-        assertEquals(3, calculator.add("1,2"));
+        assertEquals(6, calculator.add("1\n2,3"));
     }
 }
